372e6444cb584ff77c0b4e3accbbd3bd260879df
ChaitanyDewangan <105163011+ChaitanyDewangan@users.noreply.github.com>
2022-05-09 11:45:30 +0530

    Interactable Python Code for users

--- LaptopScarping.py
+++ LaptopScarping.py
@@ -1,10 +1,18 @@
+#-----/////////Here You can Also save Product details in MS EXCEL FILE//////////////////-----
 #import pandas as pd
-#import matplotlib.pyplot as plt
+#-----/////////Here You can Also save Product details in MS EXCEL FILE//////////////////-----
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.newegg.com/RTX-Studio-Laptops/Store/ID-1562?cm_sp=Cat_Laptops_1-_-TopNav-_-RTX-Studio-Laptops"
-# url="https://www.newegg.com/p/pl?d={search}"
+#-----Input Var
+search=input('Search Product : ')
+
+#BaseUrl
+
+
+
+#url="https://www.newegg.com/RTX-Studio-Laptops/Store/ID-1562?cm_sp=Cat_Laptops_1-_-TopNav-_-RTX-Studio-Laptops"
+url="https://www.newegg.com/p/pl?d={search}"
 
 webContent=requests.get(url)
 doc = BeautifulSoup(webContent.text, "html.parser")
@@ -17,10 +25,13 @@ product  = [x.get_text() for x in doc.find_all("a", attrs={'class':"item-title"}
 #TODO:Change here Title of 
 TITLE="Mobiles"
 productDetails={'Laptop':product,'Price':price}
+print(productDetails)
 # productDetails={'Laptop':product,'Price':price,'IMAGE':img}
 
 
+#-----/////////Here You can Also save Product details in MS EXCEL FILE//////////////////-----
 #Here Creating csv file with pandas
+
 #df=pd.DataFrame(productDetails)
 #df.to_csv(f'{TITLE}InNewegg.csv')
 
